650072a3c4145f456b070eaaec3e5e07f2712e2a
Henry Desroches <yo@henry.codes>
2017-11-03 13:53:56 -0600

    Update Day 3

---
+++ 3_tree/main.js
@@ -0,0 +1,18 @@
+anime({
+  targets: ".clouds",
+  translateX: [-190, 60],
+  loop: true,
+  easing: "linear",
+  duration: 20000,
+  autoplay: true
+});
+anime({
+  targets: ".clouds.lower",
+  translateX: [-250, 150],
+  translateY: [50, 50],
+  scale: [0.5, 0.5],
+  duration: 60000,
+  easing: "linear",
+  loop: true,
+  autoplay: true
+});
