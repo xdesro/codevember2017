b63323cb5520ca56c9146cb8e60bbbb28ecdd370
Henry Desroches <yo@henry.codes>
2017-11-03 11:27:54 -0600

    Update gitignore, beautify day 2 JS, start on day 3

--- 2_cake/app.js
+++ 2_cake/app.js
@@ -1,49 +1,49 @@
 const thingsToSay = [
-    "It's a lie!",
-    "Huge success!",
-    "You monster.",
-    "This is your fault!",
-    "You are not a good person!",
-    "Seriously, wow.",
-    "This was a triumph"
+  "It's a lie!",
+  "Huge success!",
+  "You monster.",
+  "This is your fault!",
+  "You are not a good person!",
+  "Seriously, wow.",
+  "This was a triumph"
 ]
-$('.plate').click(function() {
-    const windowWidth = $(window).width();
-    const windowHeight = $(window).height();
-    const plateWidth = $(this).width();
-    const plateHeight = $(this).height();
-    
-    let newTop = Math.random() * windowHeight;
-    let newLeft = Math.random() * windowWidth;
+$('.plate').click(function () {
+  const windowWidth = $(window).width();
+  const windowHeight = $(window).height();
+  const plateWidth = $(this).width();
+  const plateHeight = $(this).height();
 
-    (newTop > (windowHeight / 2)) ? newTop -= 2 * plateHeight : newTop += plateHeight;
-    (newLeft > (windowWidth / 2)) ? newLeft -= 2 * plateWidth : newLeft += plateWidth;
+  let newTop = Math.random() * windowHeight;
+  let newLeft = Math.random() * windowWidth;
 
-    if (!$('#alert').hasClass('first')) {
-        $('#alert').addClass('first');
-        $('#alert .text').text(thingsToSay[0]);
-        $('.invitation').hide();
+  (newTop > (windowHeight / 2)) ? newTop -= 2 * plateHeight: newTop += plateHeight;
+  (newLeft > (windowWidth / 2)) ? newLeft -= 2 * plateWidth: newLeft += plateWidth;
 
-    } else {
-        const randomThingToSay = thingsToSay[Math.floor(Math.random() * thingsToSay.length)];
-        $('#alert .text').text(randomThingToSay);
-    }
-    $('#alert').addClass('active');
+  if (!$('#alert').hasClass('first')) {
+    $('#alert').addClass('first');
+    $('#alert .text').text(thingsToSay[0]);
+    $('.invitation').hide();
 
-    const flash = setTimeout(function() {
-        $('#alert').removeClass('active');
-        clearTimeout(flash);
-        
+  } else {
+    const randomThingToSay = thingsToSay[Math.floor(Math.random() * thingsToSay.length)];
+    $('#alert .text').text(randomThingToSay);
+  }
+  $('#alert').addClass('active');
 
-    }, 200);
-    
-    ;
-    
+  const flash = setTimeout(function () {
+    $('#alert').removeClass('active');
+    clearTimeout(flash);
 
 
-    $(this).css({
-        "top": newTop,
-        "left": newLeft,
-        "right": 'auto'
-    })
+  }, 200);
+
+  ;
+
+
+
+  $(this).css({
+    "top": newTop,
+    "left": newLeft,
+    "right": 'auto'
+  })
 })
