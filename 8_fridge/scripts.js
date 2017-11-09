0f4591560a9ce399a44de72530d0c48cdaf4a669
Henry Desroches <yo@henry.codes>
2017-11-09 08:24:07 -0700

    Finish day 8

---
+++ 8_fridge/scripts.js
@@ -0,0 +1,9 @@
+anime({
+  targets: ".scene",
+  rotateY: ['-51deg', '-61deg'],
+  rotateX: ['-28deg', '-27deg'],
+  loop: true,
+  direction: 'alternate',
+  easing: 'easeInOutQuad',
+  duration: 3000
+})
